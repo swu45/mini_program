1fa096cef015875a32fe8c0bd769c22fa645df6c
swu45 <swu45@aliyun.com>
2017-07-06 17:07:51 +0800

    补充参考文档

--- string_substitute.py
+++ string_substitute.py
@@ -14,3 +14,8 @@ import re
 input_string = u"王天 21siow'涵"
 r1 = u'[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~ ]+'
 print re.sub(r1, '', input_string)
+
+# 参考文档：
+# 1.详解Python中re.sub（https://www.crifan.com/python_re_sub_detailed_introduction/）
+# 2.用python进行数据预处理，过滤特殊符号，英文和数字。（适用于中文分词）
+#   （http://blog.csdn.net/a1b2c3d4123456/article/details/47910649?locationNum=10）

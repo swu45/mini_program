5cf31aeada13c9a0f2a4364de1b2010d1ba7215f
swu45 <swu45@aliyun.com>
2017-07-11 16:57:02 +0800

    Update mail_receive.py

--- mail_receive.py
+++ mail_receive.py
@@ -82,11 +82,11 @@ def print_info(msg, indent=0):
         else:
             # 不是文本,作为附件处理:
             print('%sAttachment: %s' % ('  ' * indent, content_type))
-            for par in msg.walk():
-                name = decode_str(par.get_filename())
+            for part in msg.walk():
+                name = decode_str(part.get_filename())
                 if name:
                     print 'attachment name is: %s' % name
-                    data = par.get_payload(decode=True)
+                    data = part.get_payload(decode=True)
                     f = open(name, 'wb')
                     f.write(data)    # 下载附件
                     f.close()
